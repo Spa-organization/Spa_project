3392f3965441befdcfff6c667558485f1e3e9800
quasydwekat2 <160338909+quasydwekat2@users.noreply.github.com>
2024-03-25 01:58:39 +0200

    update

--- src/main/java/basic/Main.java
+++ src/main/java/basic/Main.java
@@ -2,7 +2,7 @@ package basic;
 
 import Controller.Starter;
 public class Main {
-
+//
     public static void main(String[] args) {
         Starter starter= new Starter();
         starter.HomePage();
